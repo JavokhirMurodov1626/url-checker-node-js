11f9ed2cabdeaff835b1c413fcf10478a653efe2
Murodov Javohir <javohir.murodov.9238@gmail.com>
2024-05-21 17:46:09 +0500

    handle prisma errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,56 @@
-const errorController = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const status = err.status || "error";
+const AppError = require("../utils/appError");
 
-  res.status(statusCode).json({
-    status,
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
     message: err.message,
+    stack: err.stack,
+    error: err,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send messager to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    //1) log error
+    console.log("Error", err);
+
+    //2) send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
+const handlePrismaErrors = (err) => {
+  console.log(err);
+  if (err.code === "P2002") {
+    let customError = new AppError(
+      `${err.meta.target[0]} already exists. Please use another ${err.meta.target[0]}`,
+      400
+    );
+    return customError;
+  }
+};
+
+const errorController = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    const modifiedError = handlePrismaErrors(err);
+    sendErrorProd(modifiedError, res);
+  }
 };
 
 module.exports = errorController;
--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -2,51 +2,40 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
 const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
 
 const prisma = new PrismaClient();
 const signUpController = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { full_name, email, password } = req.body;
 
-  // check if user exists
-  const existingUser = await prisma.user.findUnique({
-    where: {
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const newUser = await prisma.user.create({
+    data: {
+      full_name,
       email,
+      password: hashedPassword,
+    },
+    select: {
+      user_id: true,
+      full_name: true,
+      email: true,
     },
   });
 
-  // if user does not exist
-  if (!existingUser) {
-    return next(new AppError("User does not exist!", 404));
-  }
-
-  // check if password is correct
-  const isPasswordCorrect = await bcrypt.compare(
-    password,
-    existingUser.password
-  );
-
-  // if password is incorrect
-  if (!isPasswordCorrect) {
-    return next(new AppError("Incorrect password!", 401));
-  }
-
   const token = jwt.sign(
-    { userId: existingUser.user_id, email: existingUser.email },
+    { userId: newUser.user_id, email: newUser.email },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN }
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
   );
 
-  res.status(200).json({
-    message: "User logged in successfully!",
+  res.status(201).json({
+    message: "User created successfully!",
     token,
-    code: 200,
+    code: 201,
     data: {
-      user: {
-        user_id: existingUser.user_id,
-        full_name: existingUser.full_name,
-        email: existingUser.email,
-      },
+      user: newUser,
     },
   });
 });
