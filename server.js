cfc75889f4d8b66ccd93d0228163fec73357f1ec
Murodov Javohir <javohir.murodov.9238@gmail.com>
2024-05-22 09:12:03 +0500

    add uncaughtExeption and unhandledRejection error handlers

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app');
+const app = require("./app");
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
 });
